76a5581e2ca50258c0c5466b4e88d2aa287ece32
LucasBissolotti <lucasssbissolotti@gmail.com>
2025-06-19 02:15:31 -0300

    Criação da versão em inglês

---
+++ local/aguia/lang/en/local_aguia.php
@@ -0,0 +1,48 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+// Plugin name
+$string['pluginname'] = 'AGUIA - Accessibility and Guide for People with Visual Impairment';
+$string['aguia:addinstance'] = 'Add a new AGUIA block';
+$string['aguia:myaddinstance'] = 'Add a new AGUIA block to My Moodle page';
+
+// Accessibility panel title
+$string['accessibilitypanel'] = 'AGUIA Accessibility Panel';
+
+// Panel options
+$string['highcontrastmode'] = 'High Contrast Mode';
+$string['togglehighcontrast'] = 'Toggle High Contrast';
+$string['fontsize'] = 'Font Size';
+$string['increasefontsize'] = 'Increase Font Size';
+$string['decreasefontsize'] = 'Decrease Font Size';
+$string['linespacing'] = 'Line Spacing';
+$string['togglelinespacing'] = 'Toggle Line Spacing';
+$string['auditoryfeedback'] = 'Auditory Feedback';
+$string['toggleauditoryfeedback'] = 'Toggle Auditory Feedback';
+$string['keyboardshortcuts'] = 'Keyboard Shortcuts';
+$string['viewshortcuts'] = 'View Keyboard Shortcuts';
+$string['panel_close'] = 'Close Panel';
+
+// Auditory feedback messages
+$string['audio_page_loaded'] = 'Page loaded';
+$string['audio_action_completed'] = 'Action completed';
+$string['audio_modal_opened'] = 'Popup window opened';
+
+// Logs
+$string['logaction:toggle_high_contrast'] = 'Toggled AGUIA high contrast mode to {$a}.';
+$string['logaction:change_font_size'] = 'Changed AGUIA font size to level {$a}.';
+$string['logaction:toggle_line_spacing'] = 'Changed AGUIA line spacing to {$a}.';
+$string['logaction:toggle_auditory_feedback'] = 'Changed AGUIA auditory feedback to {$a}.';
+$string['logaction:shortcut_used'] = 'Used keyboard shortcut: {$a}.';
+$string['logaction:panel_opened'] = 'Opened AGUIA accessibility panel.';
+$string['logaction:panel_closed'] = 'Closed AGUIA accessibility panel.';
+
+// Admin settings (settings.php)
+$string['enableplugin'] = 'Enable AGUIA plugin';
+$string['enableplugin_desc'] = 'Check to enable the AGUIA plugin site-wide.';
+$string['default_highcontrast'] = 'Default High Contrast Mode';
+$string['default_highcontrast_desc'] = 'Set whether high contrast mode should be enabled by default for new users.';
+
+$string['enabled'] = 'Enabled';
+$string['disabled'] = 'Disabled';
